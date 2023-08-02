98c5413122268de5275dd7b4887c2265a4943c08
Sonika Acharya <sonika@fusemachines.com>
2023-08-02 16:27:13 +0545

    added functions add, subtract and multiply

---
+++ mathops.py
@@ -0,0 +1,14 @@
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+
+print("Sum is:", add(5,10))
+print("Difference is:", sub(15,10))
+print("Product is:", mul(5,10))
