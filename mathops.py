cfa05fb49df3b480cde9d911b4dfd451ecb8341d
Sonika Acharya <sonika@fusemachines.com>
2023-08-03 10:47:59 +0545

    Updated code with docstring

--- mathops.py
+++ mathops.py
@@ -1,15 +1,66 @@
+def add(a, b):
+    """
+    Add two numbers and return the result.
 
-def add(a,b):
-    return a+b
+    This function takes two numeric values, 'a' and 'b', and returns their sum.
 
-def sub(a,b):
-    return a-b
+    Args:
+        a (int or float): The first number.
+        b (int or float): The second number.
 
-def mul(a,b):
-    return a*b
+    Returns:
+        int or float: The sum of 'a' and 'b'.
+    """
+    return a + b
 
-def div(a,b):
-    return a/b
+def sub(a, b):
+    """
+    Subtract two numbers and return the result.
+
+    This function takes two numeric values, 'a' and 'b', and returns their difference.
+
+    Args:
+        a (int or float): The first number.
+        b (int or float): The second number.
+
+    Returns:
+        int or float: The difference of 'a' and 'b'.
+    """
+    return a - b
+
+def mul(a, b):
+    """
+    Multiply two numbers and return the result.
+
+    This function takes two numeric values, 'a' and 'b', and returns their product.
+
+    Args:
+        a (int or float): The first number.
+        b (int or float): The second number.
+
+    Returns:
+        int or float: The product of 'a' and 'b'.
+    """
+    return a * b
+
+def div(a, b):
+    """
+    Divide 'a' by 'b' and return the result.
+
+    This function takes two numeric values, 'a' (dividend) and 'b' (divisor), and returns the result
+    of dividing 'a' by 'b'.
+
+    Args:
+        a (int or float): The number to be divided.
+        b (int or float): The number to divide by.
+
+    Returns:
+        float: The result of the division.
+
+    Raises:
+        ZeroDivisionError: If 'b' is 0.
+    """
+    return a / b
 
 print("Sum is:", add(5,10))
 print("Difference is:", sub(15,10))
